4fae73c35e9233ee000ea74bc2c636fdaee709db
KSJ0413 <rlatjdwls2525@gmail.com>
2024-04-18 00:35:42 +0900

    주식 데이터 API키 발급, API 연결 , 데이터 파싱

--- app/src/main/java/com/example/stockwidget___process/StockWidget.kt
+++ app/src/main/java/com/example/stockwidget___process/StockWidget.kt
@@ -4,12 +4,22 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 /**
  * Implementation of App Widget functionality.
  */
 class StockWidget : AppWidgetProvider() {
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -25,7 +35,50 @@ class StockWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val endPoint = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService"
+    val apiKey = "B%2BSxs95%2F%2BrJUXfLvraUCT3Hz%2FGxcpKGpUEX17%2BQQ7cFJED07MQ34vQn%2F%2FWj54mgzQkKFH9pFuBKpI0zdu9eSIg%3D%3D"
+
+    // API 요청을 위한 URL 생성
+    val urlString = "$endPoint?serviceKey=$apiKey"
+
+    // URL 객체 생성
+    val url = URL(urlString)
+
+    // HttpURLConnection 객체 생성
+    val connection = url.openConnection() as HttpURLConnection
+
+    // 요청 방식 설정 (GET 또는 POST)
+    connection.requestMethod = "GET"
+
+    // 연결 시도
+    connection.connect()
+
+    // 응답 코드 확인
+    val responseCode = connection.responseCode
+    if (responseCode == HttpURLConnection.HTTP_OK) {
+        // 응답이 성공적으로 왔을 때의 처리
+        val inputStream = connection.inputStream
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val response = reader.readText()
+
+        // JSON 파싱
+        val jsonObject = JSONObject(response)
+
+        // 필요한 데이터 추출
+        val stockPrice = jsonObject.getDouble("price")
+        val changePercent = jsonObject.getDouble("changePercent")
+    } else {
+        // 응답이 실패했을 때의 처리
+        // ...
+    }
+
+    connection.disconnect()
+
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.stock_widget)
